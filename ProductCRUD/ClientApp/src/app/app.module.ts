ef308f7a3e088fe1795bb8f62900ccf42ed29f6c
Tiago R. Ladeia <dtiagorl@gmail.com>
2020-03-11 00:29:12 -0300

    Remoção de arquivos do template de projeto

--- ProductCRUD/ClientApp/src/app/app.module.ts
+++ ProductCRUD/ClientApp/src/app/app.module.ts
@@ -5,10 +5,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { ProdutoComponent } from './produtos/produto/produto.component';
 import { ProdutoListaComponent } from './produtos/produto-lista/produto-lista.component';
@@ -18,10 +14,6 @@ import { ProdutoService } from './shared/produto.service';
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ProdutosComponent,
     ProdutoComponent,
     ProdutoListaComponent
@@ -31,9 +23,9 @@ import { ProdutoService } from './shared/produto.service';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: ProdutosComponent, pathMatch: 'full' },
+      //{ path: 'counter', component: CounterComponent },
+      //{ path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [ProdutoService],
